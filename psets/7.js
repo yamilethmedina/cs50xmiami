605bdb274ba311a4dc6caaa4660b267d9f610073
Yami <yamilethmedina1@gmail.com>
2015-09-08 19:57:48 -0400

    completed pset 7

--- psets/7.js
+++ psets/7.js
@@ -16,13 +16,42 @@ fixStart('babble'): 'ba**le'
 function fixStart(s) {
   // write your solution here...
 
-  new regExp([^^], gi);
-  s = s.replace(/[^^]l/g, '')
+/*  new regExp([^^], gi);
+  s = s.replace(/[^^]l/g, '') */
 
   /* var toReplace = s[0];
   s = s.replace(toReplace, '*') */
 
+// var regexp = /(s[0])(*)/g; //Example
+
+/* function escapeRegExp(string) {
+    return string.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+} */
+
+
+// var find = s[0];
+//if (s[0] === new RegExp(s[0], 'gi'))
+// {
+//   console.log("It worked");
+// }
+//   var re = new RegExp(s[0], 'gi');
+//
+// for (var i = 1; i < s.length; i++)
+// {
+//   //  console.log(s[i]);
+//     if (false) //s[i] == re = new RegExp(s[0], 'gi')
+//       {
+//         s[i] = '*';
+//         console.log(s[i]);
+//       }
+
+  var re = new RegExp(s[0], 'gi');
+  var substring = s.substring(1, s.length);
+  s = s[0] + substring.replace(re, '*');
+
+//equals string = "something foo bar red  foo bar RED red pink   foo bar RED red"
+
   console.log(s);
 }
 
-fixStart("Gotta get him out my hair")
+fixStart("Sally sells seashells by the seashore");
