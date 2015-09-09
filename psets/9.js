470295b3ad0d60b867fd4f6e5f2c50efe887541e
Yami <yamilethmedina1@gmail.com>
2015-09-08 21:36:04 -0400

    going home, pset9 half-finished

--- psets/9.js
+++ psets/9.js
@@ -17,4 +17,40 @@ For example:
 
 function notBad(sentence) {
   // write your solution here...
+
+  var not = sentence.indexOf('not');
+  // sentence.search(/not/wi);
+  // console.log(not);
+  var bad = sentence.indexOf('bad');
+  // sentence.search(/bad/wi);
+  // console.log(bad);
+
+  if (not === -1 && bad === -1 && not < bad)
+
+  {
+
+    // substitute not...bad with good
+
+    var sentence1 = sentence.slice(0, not);
+    var sentence2 = sentence.slice(bad + 3);
+    sentence = sentence1 + 'good' + sentence2;
+    // var substring = sentence.substring(not - 1, bad + 1);
+    // console.log(substring);
+    // sentence = str.replace(/substring/i, "good");
+    console.log(sentence);
+
+    // substring = "good";
+    // console.log(substring);
+  //  sentence = sentence + substring;
+
+
+  }
+  else
+  {
+    console.log(sentence);
+  }
 }
+
+notBad("This new Justin Bieber song is not too bad.");
+notBad("This ice cream is really bad.");
+notBad("Stop, drop, shut em down open up shop.");
