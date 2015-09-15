58f6ccd9acc039d5f4a0e4716e323bf61f266863
Yami <yamilethmedina1@gmail.com>
2015-09-15 18:46:39 -0400

    pset 16 complete

--- psets/16.js
+++ psets/16.js
@@ -11,3 +11,28 @@ and log the result to make sure it works.
 */
 
 // write your solution here...
+
+function helloWorld(languageCode) {
+
+  if (languageCode == "es")
+  {
+    console.log("Hola, Mundo!");
+  }
+  else if (languageCode == "en")
+  {
+    console.log("Hello, World!");
+  }
+  else if (languageCode == "de")
+  {
+    console.log("Hallo, Welt!");
+  }
+  else
+  {
+    console.log("Hello, World!");
+  }
+}
+
+helloWorld("en");
+helloWorld("es");
+helloWorld("de");
+helloWorld("ru");
