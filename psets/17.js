d9e3143385e5d13476c355cefe7a7173063c73dc
Yami <yamilethmedina1@gmail.com>
2015-09-15 18:54:16 -0400

    pset17 complete (including bonus)

--- psets/17.js
+++ psets/17.js
@@ -10,3 +10,35 @@ Call that function for a few different scores and log the result to make sure it
 */
 
 // write your solution here...
+
+function pluralize(noun, number) {
+
+  if (number > 1)
+  {
+    if (noun == "goose")
+    {
+      console.log(number + " geese");
+    }
+
+    else if (noun == "sheep")
+    {
+      console.log(number + " " + noun);
+    }
+
+    else
+    {
+    console.log(number + " " + noun + "s");
+    }
+  }
+  else
+  {
+
+      console.log(number + " " + noun);
+
+  }
+}
+
+pluralize("dog", 1);
+pluralize("cat", 5);
+pluralize("goose", 3);
+pluralize("sheep", 1);
