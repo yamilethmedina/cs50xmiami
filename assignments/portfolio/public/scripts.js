bc14399de41f679256e2416df4561f195a8a1b7e
Yami <yamilethmedina1@gmail.com>
2015-09-26 23:32:31 -0400

    got half of final project working with api

--- assignments/portfolio/public/scripts.js
+++ assignments/portfolio/public/scripts.js
@@ -1,6 +1,6 @@
 
 
-
+var $ = require('jquery');
 var Spotify = require('../node_modules/spotify-web-api-js');
 var s = new Spotify();
 
@@ -12,134 +12,65 @@ console.log("Hello");
 //   redirectUri : 'http://localhost:3000/callback'
 // });
 
-var searchButton = document.getElementById('search');
-if(searchButton) {
-    searchButton.addEventListener('click', getOriginalArtist, false);
+// closure waiting for page to load event
+// window.addEventListener('click', getOriginalArtist)
 
+$(document).ready(function($) {
+  window.addEventListener('click', function() {
+    var searchButton = document.getElementById('search');
+    //console.log(searchButton);
+    getOriginalArtist();
+  });
+
+});
 
 function getOriginalArtist() {
   var originalArtist = document.getElementById('originalartist').value;
   console.log(originalArtist);
   searchArtists(originalArtist);
+}
 
-  }
+var originalArtistId;
+var relatedArtists;
+var relatedArtistsLength;
+function searchArtists(originalArtist) {
 
-  function searchArtists(originalArtist) {
-    var originalArtistId =  $.getJSON("https://api.spotify.com/v1/search?type=artist&q=" + originalArtist);
+
+  $.getJSON("https://api.spotify.com/v1/search?type=artist&q=" + originalArtist, function(json) {
+   $('#artist').html('<p>'+ '<img src="' + json.artists.items[0].images[2].url + '" height="100" width="100" /> ' + json.artists.items[0].name +'</p>');
+
+    originalArtistId = json.artists.items[0].id;
+
     console.log(originalArtistId);
 
-  //  $.getJSON("https://api.spotify.com/v1/search?type=artist&q=" + originalArtist, function(json) {
-  //    $('#artist').html('<p>'+ '<img src="' + json.artists.items[0].images[2].url + '" height="100" width="100" /> ' + json.artists.items[0].name +'</p>');
-  //    var originalArtistId
-   //
-  //    relatedArtists(json.artists.items[0].id);
-   };
- }
+    //everything should happen in the callback
+    relatedArtists = s.getArtistRelatedArtists(originalArtistId);
+    console.log(relatedArtists);
+    relatedArtistsLength = relatedArtists.length;
+    console.log(relatedArtistsLength);
+  });
+}
+
+// relatedArtists = s.getArtistRelatedArtists(originalArtistId, function(err, data) {
+//   if (err) console.error(err);
+//   else console.log('Related Artists', data)
+// });
 
 
-  // function searchArtists(originalArtist) {
-  //   $.getJSON("https://api.spotify.com/v1/search?type=artist&q=" + originalArtist, function(json) {
-  //     $('#artist').html('<p>'+ '<img src="' + json.artists.items[0].images[2].url + '" height="100" width="100" /> ' + json.artists.items[0].name +'</p>');
-  //
-  //     relatedArtists(json.artists.items[0].id);
-  //   });
-  // }
+
+  for (var i = 0; i < relatedArtistsLength; i++) {
+
+      //search for related artist ID to get top song
+      console.log(relatedArtists[i].items[0].name);
+      var relatedArtistId = searchArtists(relatedArtists[i].items[0].name);
+      console.log(relatedArtistId[i].items[0].name);
+
+      //get top song from that related artist
+      var relatedArtistTopTracks = s.getArtistTopTracks(relatedArtistId[i], US);
 
 
-//
-// spotifyApi.getArtist('originalArtist') {
-//   .then(function(data) {
-//     ('#artist').html('<p>'+ '<img src="' + data.body.items[0].images[2].url + '" height="100" width="100" /> ' + json.artists.items[0].name +'</p>', data.body);
-//   }, function(err) {
-//     console.error(err);
-//   });
-//
-//   $.getJSON(spotifyApi.getArtist('originalArtist'), function(json) {
-//     $('#artist').html('<p>'+ '<img src="' + json.artists.items[0].images[2].url + '" height="100" width="100" /> ' + json.artists.items[0].name +'</p>');
+      // reset html of related artist for new search
+      $('#related-artist').html('');
 
-
-//
-// // Get an artist
-// var originalArtistName = spotifyApi.getArtist(originalArtistId)
-//   .then(function(data) {
-//     console.log('Artist information', data.body);
-//   }, function(err) {
-//     console.error(err);
-//   });
-//
-// // Get artists related to an artist
-// var relatedArtists = spotifyApi.getArtistRelatedArtists(originalArtistName)
-//   .then(function(data) {
-//     console.log(data.body);
-//   }, function(err) {
-//     done(err);
-//   });
-//
-//
-//
-// for (var i=0; i < relatedArtists.length; i++)
-// {
-// // Get an artist's top tracks
-//   var relatedArtistsTopTracks = spotifyApi.getArtistTopTracks(relatedArtists[i], 'US')
-//     .then(function(data) {
-//       console.log(data.body);
-//       }, function(err) {
-//         console.log('Something went wrong!', err);
-//       });
-//
-//
-//
-//       // get the most popular song from each related artist
-//     var relatedArtistsTopSong = relatedArtistsTopTracks[i][0];
-//     console.log(relatedArtistsTopSong);
-//
-// };
-
-// var searchButton = document.getElementById('search');
-// if(searchButton) {
-//     searchButton.addEventListener('click', getOriginalArtist, false);
-//
-//
-// function getOriginalArtist() {
-//   var originalArtist = document.getElementById('originalartist').value;
-//   searchArtists(originalArtist);
-// }
-//
-// function searchArtists(originalArtist) {
-//   $.getJSON("https://api.spotify.com/v1/search?type=artist&q=" + originalArtist, function(json) {
-//     $('#artist').html('<p>'+ '<img src="' + json.artists.items[0].images[2].url + '" height="100" width="100" /> ' + json.artists.items[0].name +'</p>');
-//
-//     relatedArtists(json.artists.items[0].id);
-//   });
-// }
-//
-// function relatedArtists(originalArtistId) {
-//   $.getJSON("https://api.spotify.com/v1/artists/" + originalArtistId + "/related-artists", function(json) {
-//     if(json.artists.length > 0) {
-//       var artistsLength = json.artists.length;
-//
-//       //search for related artist ID to get top song
-//       var relatedArtistId = searchArtists(artists[i]);
-//
-//       //get top song from that related artist
-//       function relatedArtistTopSong(relatedArtistId) {
-//         $.getJSON("https://api.spotify.com/v1/artists/" + relatedArtistId + "/top-tracks?country=US", function(json) {
-//           if(json.tracks.length > 0) {
-//             var tracksLength = json.track.length;
-//
-//       // reset html of related artist for new search
-//       $('#related-artist').html('');
-//
-//       //reset html of tracks for new search
-//       $('#related-artist-track').html('');
-//
-//       // loop through related artists, append name of each
-//       for(var i = 0; i < artistsLength; i++) {
-//         $('#related-artist').append('<p>'+ '<img src="' + json.artists[i].images[2].url + '" height="100" width="100" /> ' + json.artists[i].name + '<i>' + json.tracks[0].name + '</i></p>');
-//
-//       //append the top track
-//       }
-//     }
-//   });
-// }
-// }
+      $('#related-artist').append('<p>'+ '<img src="' + relatedArtists[i].images[2].url + '" height="100" width="100" /> ' + relatedArtists[i].name + '<i> \"' + relatedArtists.tracks[0].name + '\"</i></p>');
+    }
